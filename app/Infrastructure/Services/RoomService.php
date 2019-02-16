eeff0b26441611b709e2e34bb297724ff395ce33
Magnero <lin.yansen@gmail.com>
2019-02-16 07:42:11 +0700

    add new service RoomService

--- app/Infrastructure/Services/RoomService.php
+++ app/Infrastructure/Services/RoomService.php
@@ -8,8 +8,32 @@
 
 namespace App\Infrastructure\Services;
 
+use App\Infrastructure\Repositories\FirebaseRepository;
 
 class RoomService
 {
+    public function getRoomsLimitTo15Data($index = null) : array
+    {
+        $firebase = new FirebaseRepository();
+        $firebase->setReference('RoomList');
 
+        $rooms = isset($index)
+                ? $firebase->getReference()
+                            ->orderByKey()
+                            ->limitToFirst(15)
+                            ->startAt($index)
+                            ->getSnapshot()
+                            ->getValue()
+                : $firebase->getReference()
+                            ->orderByKey()
+                            ->limitToFirst(15)
+                            ->getSnapshot()
+                            ->getValue();
+        return $rooms;
+    }
+
+    public function createNewRoom()
+    {
+
+    }
 }
